f3d26958aa203f1c6f34d02204354e2ceb492423
Rafael Oliveira pires <rafaelopires@icloud.com>
2022-01-13 07:06:51 -0300

    1.0

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,35 +1,33 @@
-import UIKit
+import Foundation
 
 let finalSquare = 25
-var board = [Int] (repeating: 0, count: finalSquare + 1)
 
+var board = [Int] (repeating: 0, count: finalSquare + 1)
 board[03] = +8; board[06] = +11; board[09] = +09; board[10] = +02
 board[14] = -10; board[19] = -11; board[22] = -2; board[24] = -08
-
-print(board)
-
 var square = 0
 var diceRoll = 0
 
-while square < finalSquare {
-    //roll the dice
+gameLoop: while square != finalSquare{
     diceRoll += 1
-    
     if diceRoll == 7 {
         diceRoll = 1
     }
     
-    // Move by the rolled amount
-    square += diceRoll
-    if square < board.count {
-        // If we're still on the board, move up or down for a snake or a ladder
+    switch square + diceRoll{
+    case finalSquare:
+        print("Inside break case")
+        break gameLoop
+    case let newSquare where newSquare > finalSquare:
+        print("Inside continue swith case")
+        continue gameLoop
+    default:
+        square += diceRoll
         square += board[square]
-        print(square)
-        
+
     }
 }
 print("Game over!")
-
 
 
 
